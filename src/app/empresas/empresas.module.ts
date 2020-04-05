cbda1ea88f03d4c2169acb92d94f4a2a714f5ce9
Carlos Alejandro Hernandez Mejia <alejandromejia2013.27@outlook.com>
2020-04-05 14:14:12 -0300

    Add 'empresas' page

---
+++ src/app/empresas/empresas.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { EmpresasPageRoutingModule } from './empresas-routing.module';
+
+import { EmpresasPage } from './empresas.page';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    EmpresasPageRoutingModule
+  ],
+  declarations: [EmpresasPage]
+})
+export class EmpresasPageModule {}
