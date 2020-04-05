278b43329d98c621fccf84b1cb31d36c041da43d
Carlos Alejandro Hernandez Mejia <alejandromejia2013.27@outlook.com>
2020-04-05 05:19:18 -0300

    Add slides view

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import { LayoutModule } from '@angular/cdk/layout';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    IonicModule,
+    CommonModule,
+    LayoutModule,
+  ],
+  exports: [
+    IonicModule
+  ]
+})
+export class SharedModule { }
