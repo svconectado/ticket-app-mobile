78a9cc9e7ae06d01753c8d0e7ecb6410aca2ff9e
Carlos Alejandro Hernandez Mejia <alejandromejia2013.27@outlook.com>
2020-04-11 22:59:55 -0300

    Add 'ticket' page

--- src/app/ticket/ticket.module.ts
+++ src/app/ticket/ticket.module.ts
@@ -2,17 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
-import { IonicModule } from '@ionic/angular';
-
 import { TicketPageRoutingModule } from './ticket-routing.module';
 
 import { TicketPage } from './ticket.page';
+import { SharedModule } from '@shared/shared.module';
 
 @NgModule({
   imports: [
-    CommonModule,
     FormsModule,
-    IonicModule,
+    CommonModule,
+    SharedModule,
     TicketPageRoutingModule
   ],
   declarations: [TicketPage]
